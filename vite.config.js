79eacdc05fa15a589369e8896f4ff6d818712e64
Ducheved <ducheved@mail.ru>
2024-03-10 18:54:53 +0300

    ( =ノωヽ=) use docs https://vitejs.dev/guide/static-deploy#github-pages

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,10 @@ import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 import devtools from 'solid-devtools/vite';
 import { fileURLToPath, URL } from 'node:url';
-import ghpages from 'vite-plugin-gh-pages';
 
 export default defineConfig({
-  plugins: [devtools(), solidPlugin(), ghpages()],
+  base: '/Cute.Toxicat/',
+  plugins: [devtools(), solidPlugin()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
